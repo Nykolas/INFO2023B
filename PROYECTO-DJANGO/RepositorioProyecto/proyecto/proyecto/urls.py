b3d77e95cbebe7bac8c19905dae0dccdb1e0f8f7
Nicolas <nicotortosa@gmail.com>
2023-11-21 11:52:38 -0300

    apps

--- PROYECTO-DJANGO/RepositorioProyecto/proyecto/proyecto/urls.py
+++ PROYECTO-DJANGO/RepositorioProyecto/proyecto/proyecto/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -22,4 +22,7 @@ urlpatterns = [
 
     path('', views.Home, name ='inicio'),
     path('contacto', views.Contacto, name ='contacto'),
+
+    #APP NOTICIAS
+    path('noticias/', include('apps.noticias.urls')),
 ]
