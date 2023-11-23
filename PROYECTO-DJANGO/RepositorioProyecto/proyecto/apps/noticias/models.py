49f75f1c89919ffd5967493d7b4ac0f64ce38c41
Nicolas <nicotortosa@gmail.com>
2023-11-23 11:27:16 -0300

    migraciones

--- PROYECTO-DJANGO/RepositorioProyecto/proyecto/apps/noticias/models.py
+++ PROYECTO-DJANGO/RepositorioProyecto/proyecto/apps/noticias/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Noticia(models.Model):
+
+	creado = models.DateTimeField(
+		'creado',
+		auto_now_add=True
+	)
+	modificado = models.DateTimeField(
+		'modificado',
+		auto_now=True
+	)
+	titulo = models.CharField(max_length = 250)
+	contenido = models.TextField()
+
+
+	def __str__(self):
+		return self.titulo
